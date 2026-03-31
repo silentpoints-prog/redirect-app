a1d67ae253764444ff0d04603e74dcf9b3fe5110
silentpoints-prog <silentpoints@gmail.com>
2026-03-31 13:57:43 +0530

    Delete api/redirect.js

--- api/redirect.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const path = req.url || "";
-  const target = "https://drludidi.wixsite.com/my-site-2" + path;
-
-  res.statusCode = 302;
-  res.setHeader("Location", target);
-  res.end();
-}

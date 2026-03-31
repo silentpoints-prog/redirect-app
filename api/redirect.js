8992ca29605307cb6442ed4ce4bdedb6adf50872
silentpoints-prog <silentpoints@gmail.com>
2026-03-31 13:53:22 +0530

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -2,8 +2,7 @@ export default function handler(req, res) {
   const path = req.url || "";
   const target = "https://drludidi.wixsite.com/my-site-2" + path;
 
-  res.writeHead(302, {
-    Location: target,
-  });
+  res.statusCode = 302;
+  res.setHeader("Location", target);
   res.end();
 }
